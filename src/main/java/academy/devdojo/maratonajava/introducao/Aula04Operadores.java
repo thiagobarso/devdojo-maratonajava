e4ded4debfe20001f857509eade536fd6fd4f69c
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-06-03 12:56:23 -0300

    15 - Operadores pt 01 - A aritmética

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numeros01 = 10;
+        int numeros02 = 20;
+        System.out.println(numeros02 + numeros01 + " Valor " + numeros02 + numeros01);
+        double resultado = (double) numeros01 / numeros02;
+        System.out.println(resultado);
+    }
+}
