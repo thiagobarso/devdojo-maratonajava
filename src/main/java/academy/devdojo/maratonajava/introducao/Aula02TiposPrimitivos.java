18a335d3e5865371f6983afc7a6464ab8018c28e
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-06-03 12:28:04 -0300

    12 - Tipos Primitivos pt 03 - casting

--- src/main/java/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,12 +4,12 @@ public class Aula02TiposPrimitivos
 {
     public static void main(String[] args) {
         //int, double, float, char, byte, short, long, boolean
-        int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
-        byte idadeByte = 10;
-        short idadeShort = 10;
+        int idade = (int) 10000000000L;
+        long numeroGrande = (long) 155.23;
+        double salarioDouble = 2000.0;
+        float salarioFloat = (float) 2500.0D;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = '\u0041';
@@ -18,5 +18,6 @@ public class Aula02TiposPrimitivos
         System.out.println("A idade é " + idade + " anos");
         System.out.println(falso);
         System.out.println("char " + caractere);
+        System.out.println(numeroGrande);
     }
 }
