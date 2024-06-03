534d250d376acbf763a0f18f38492a6d5f8784e2
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-06-03 11:50:02 -0300

    09 - Comentários

--- src/main/java/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,13 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ *  Isso é um comentário <b>javadoc</b>
+ */
 public class OlaDevDojo{
+    // Isso é o comentário de uma linha
+    /*
+    Isso é um comentário de multiplas linhas
+     */
     public static void main(String[] args) {
         System.out.println("KAMEHAMEHA");
     }
