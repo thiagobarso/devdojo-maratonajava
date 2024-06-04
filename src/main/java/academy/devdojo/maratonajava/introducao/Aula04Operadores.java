63406eb16c8544c5341429c6a688948645933a99
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-06-04 10:45:16 -0300

    16 - Operadores pt 02 - Relacionais

--- src/main/java/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -8,5 +8,23 @@ public class Aula04Operadores {
         System.out.println(numeros02 + numeros01 + " Valor " + numeros02 + numeros01);
         double resultado = (double) numeros01 / numeros02;
         System.out.println(resultado);
+
+        // %
+        int resto = 21 % 7;
+        System.out.println("21 % 7 " + resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDeferenteDez = 10 != 10;
+
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
+        System.out.println("isDezDeferenteDez "+isDezDeferenteDez);
+
     }
 }
