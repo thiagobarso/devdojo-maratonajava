92efaa2e06ce9caef1b9c4f4026df5859410eaac
unknown <thiagobarso@gmail.com>
2024-06-05 23:07:54 -0300

    39 - Orientação Objetos pt 01 - Introdução classes pt 01

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Roberto Carlos";
+        estudante.idade = 83;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
