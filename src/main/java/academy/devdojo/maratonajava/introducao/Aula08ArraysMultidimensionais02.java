4f1141e7c55defda0dc3e7a1d9f8cbd8cfb035c3
unknown <thiagobarso@gmail.com>
2024-06-12 22:28:57 -0300

    40 - Orientação Objetos - Introdução classes pt 02

--- src/main/java/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
@@ -9,7 +9,7 @@ public class Aula08ArraysMultidimensionais02 {
         arrayInt[1] = array;
         arrayInt[2] = new int[]{1,2,3,4,5,6};
 
-        int[][] arrayInt2 = {{0,0},{1,2,3},{1,2,3,4,5,6}}
+        int[][] arrayInt2 = {{0,0},{1,2,3},{1,2,3,4,5,6}};
 
         for (int[] arrayBase : arrayInt){
             System.out.println("\n-----");
---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Estudante {
+
+    public String nome = "Júnior";
+    public int idade;
+    public char sexo;
+}
