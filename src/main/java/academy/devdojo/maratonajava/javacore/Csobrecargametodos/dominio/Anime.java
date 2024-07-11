906155d3a03ae5befd180ccd16d99909193f819a
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-07-11 16:39:00 -0300

    57 - Orientação Objetos - Sobrecarga de métodos

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Csobrecargametodos/dominio/Anime.java
@@ -0,0 +1,51 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio;
+
+public class Anime {
+    private String nome;
+    private String tipo;
+    private String genero;
+    private int episodios;
+
+
+    public void init(String nome, String tipo, int episodios) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+
+    public void init(String nome, String tipo, int episodios, String genero) {
+        this.init(nome, genero, episodios);
+        this.genero = genero;
+    }
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+}
