26119e4be9e4680faadb2c543b18b25dc264cd56
unknown <thiagobarso@gmail.com>
2024-08-03 11:39:32 -0300

    78 - Orientação Objetos - Modificador final pt 02 - Tipo referência

--- src/main/java/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Comprador.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Comprador.java
@@ -1,9 +1,8 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
 
-public class Carro {
+public class Comprador {
+
     private String nome;
-    public static final double VELOCIDADE_LIMITE = 250;
-    public final Comprador COMPRADOR = new Comprador();
 
     public String getNome() {
         return nome;
@@ -11,5 +10,12 @@ public class Carro {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Comprador{" +
+                "nome='" + nome + '\'' +
+                '}';
     }
 }
