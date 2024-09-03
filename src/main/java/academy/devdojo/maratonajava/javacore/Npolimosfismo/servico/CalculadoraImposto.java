f05a2ca7a541de31359ed03ca304c5eda98e79b6
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-09-03 15:05:08 -0300

    92 - Orientação Objetos - Polimorfismo pt 03 - Parâmetros polimórficos

--- src/main/java/academy/devdojo/maratonajava/javacore/Npolimosfismo/servico/CalculadoraImposto.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/Npolimosfismo/servico/CalculadoraImposto.java
@@ -1,23 +1,14 @@
 package academy.devdojo.maratonajava.javacore.Npolimosfismo.servico;
 
-import academy.devdojo.maratonajava.javacore.Npolimosfismo.dominio.Computador;
-import academy.devdojo.maratonajava.javacore.Npolimosfismo.dominio.Tomate;
+import academy.devdojo.maratonajava.javacore.Npolimosfismo.dominio.Produto;
 
 public class CalculadoraImposto {
 
-    public static void calcularImpostoComputador(Computador computador){
+    public static void calcularImposto(Produto produto) {
         System.out.println("Relatório de imposto do computador");
-        double imposto = computador.calcularImposto();
-        System.out.println("Computador "+ computador.getNome());
-        System.out.println("Valor "+ computador.getValor());
-        System.out.println("Imposto a ser pago "+ imposto);
-    }
-
-    public static void calcularImpostoTomate(Tomate tomate){
-        System.out.println("Relatório de imposto do tomate");
-        double imposto = tomate.calcularImposto();
-        System.out.println("Tomate "+ tomate.getNome());
-        System.out.println("Valor "+ tomate.getValor());
-        System.out.println("Imposto a ser pago "+ imposto);
+        double imposto = produto.calcularImposto();
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Preço: " + produto.getValor());
+        System.out.println("Imposto a ser pago " + imposto);
     }
 }
--- src/main/java/academy/devdojo/maratonajava/javacore/Npolimosfismo/test/ProdutoTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/Npolimosfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimosfismo.test;
 
 import academy.devdojo.maratonajava.javacore.Npolimosfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimosfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.javacore.Npolimosfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimosfismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,13 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC10i7", 11000);
         Tomate tomate = new Tomate("Tomate Siciliano", 10);
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao tv = new Televisao("Samsung 50\" ", 5000);
+
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("----------------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("----------------------------");
+        CalculadoraImposto.calcularImposto(tv);
 
     }
 }
