4fb564d8b46c53c7596fbf701707b57ff08e9ea9
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-09-11 16:33:30 -0300

    139 - Classes Utilitárias - IO pt 02 - FileWriter

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Vio/test/FileWriter01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+// File
+// FileWriter
+// FileReader
+// BufferedWriter
+// BufferedReader
+public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try(FileWriter fw = new FileWriter(file,true)){
+            fw.write("O DeVDojo é lindo, é o melhor curso do Brasil.\nPulando linha");
+            fw.flush();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
