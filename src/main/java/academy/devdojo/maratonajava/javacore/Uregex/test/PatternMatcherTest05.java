80bac56fa5fb9701a42e52616ec3f6888e1388d4
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-09-11 15:03:10 -0300

    135 - Classes Utilitárias - Regex pt 05 - Pattern e Matcher - Quantificadores pt 02

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest05.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest05 {
+    public static void main(String[] args) {
+        // \d = Todos os dígitos
+        // \D = Tudo o que não for dígito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-zA-Z, digitos, _
+        // \W = Tudo o que não for incluso no \W
+        // []
+        // ? Zero ou uma
+        // * zero ou mais
+        // + uma ou mais
+        // {n,m} de n até m
+        // ()
+        // |
+        // $
+        String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+(\\.([a-zA-Z])+)+";
+        String texto = "luffy@hotmail.com, 123jotaro@gmail.com, #@!zoro@mail.br, teste@gmail.com.br, sakura@mail";
+        System.out.print("Email válido: ");
+        System.out.println("#@!zoro@mail.br".matches(regex));
+        System.out.println(texto.split(",")[1].trim());
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:  " + texto);
+        System.out.println("índice:  0123456789");
+        System.out.println("regex:   "+regex);
+        System.out.println("Posições encontradas");
+        while(matcher.find()){
+            System.out.print(matcher.start()+" "+matcher.group()+"\n");
+        }
+
+    }
+}
