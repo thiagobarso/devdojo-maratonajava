7d6389193e7ceff5d352937159bd9faabef575fb
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-09-12 11:48:33 -0300

    143 - Classes Utilitárias - IO pt 06 - File para diretórios

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Vio/test/FileTest02.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+
+public class FileTest02 {
+    public static void main(String[] args) throws IOException {
+        File fileDiretorio = new File("pasta");
+        boolean isDiretorioCreated = fileDiretorio.mkdir();
+        System.out.println("Pasta criada? " + isDiretorioCreated);
+        File fileArquivoDiretorio = new File(fileDiretorio, "arquivo.txt");
+        boolean isFileCreated = fileArquivoDiretorio.createNewFile();
+        System.out.println("arquivo.txt criado? " + isFileCreated);
+
+        File fileRenamed = new File(fileDiretorio, "arquivo_renomeado.txt");
+        boolean isRenamed = fileArquivoDiretorio.renameTo(fileRenamed);
+        System.out.println("arquivo.txt renomeado? " + isRenamed);
+
+        File diretorioRenamed = new File("pasta2");
+        boolean isDiretorioRenomeado = fileDiretorio.renameTo(diretorioRenamed);
+        System.out.println("Diretório 'pasta' renomeado? " + isDiretorioRenomeado);
+    }
+}
