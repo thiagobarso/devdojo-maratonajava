d5886985556e7d8c4ba9428e0bcd6057253c8d57
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-09-16 16:14:53 -0300

    159 - Classes Utilitárias - Serialization pt 01

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.Xserializacao.test;
+
+
+import academy.devdojo.maratonajava.javacore.Xserializacao.dominio.Aluno;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SerializacaoTest01 {
+    public static void main(String[] args) {
+//        Aluno aluno = new Aluno(1L, "Suane", "123456");
+//        serializar(aluno);
+        desserializar();
+    }
+
+    private static void serializar(Aluno aluno) {
+        Path path = Paths.get("pasta/aluno.ser");
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))) {
+            oos.writeObject(aluno);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void desserializar() {
+        Path path = Paths.get("pasta/aluno.ser");
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))) {
+            Aluno aluno = (Aluno) ois.readObject();
+            System.out.println(aluno);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
