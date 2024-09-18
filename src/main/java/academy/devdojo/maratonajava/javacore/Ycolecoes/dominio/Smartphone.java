7016036d8c56ae25ea9e90104c19025867bcd153
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-09-18 20:01:50 -0300

    168 - Coleções pt 08 - List pt 03

--- src/main/java/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -10,6 +10,14 @@ public class Smartphone {
         this.marca = marca;
     }
 
+    @Override
+    public String toString() {
+        return "Smartphone{" +
+                "serialNumber='" + serialNumber + '\'' +
+                ", marca='" + marca + '\'' +
+                '}';
+    }
+
     // Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
     // Simétrico: para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
     // Transitividade: para x,y,z diferentes de null, se x.equals(y) == true, e x.equals(z) == true logo, y.equals(z) == true
