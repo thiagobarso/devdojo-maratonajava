9f94903464e994e6c36bdf5e829232a5f71d4757
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-09-23 15:12:08 -0300

    188 - Generics pt 06 - Métodos Genéricos

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Zgenerics/test/MetodoGenericoTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+
+import java.util.List;
+
+public class MetodoGenericoTest01 {
+    public static void main(String[] args) {
+        List<Barco> barcoList = criarArrayComUmObjeto(new Barco("Titanic"));
+        System.out.println(barcoList);
+    }
+
+    private static <T> List<T> criarArrayComUmObjeto(T t){
+        return List.of(t);
+    }
+
+//    private static <T extends Comparable<T>> List<T> criarArrayComUmObjeto(T t){
+//        return List.of(t);
+//    }
+}
