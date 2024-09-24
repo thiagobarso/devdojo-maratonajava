5b835d4f890d8560f5b484551de6caccb088fddc
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-09-24 13:57:00 -0300

    199 - Method Reference pt 02 - Referência a métodos não estáticos

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Reference to an instance method of a particular object
+public class MethodReferenceTest02 {
+    public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berseck", 43), new Anime("One piece", 100), new Anime("Naruto", 500)));
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+        System.out.println(animeList);
+
+    }
+}
---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest03.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+
+//Reference to an instance method of a particular object
+public class MethodReferenceTest03 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>(List.of("Rimuru", "Veldora", "Hikimuru"));
+        list.sort(String::compareTo);
+        System.out.println(list);
+
+        Function<String, Integer> numStringToInteger = Integer::parseInt;
+        Integer num = numStringToInteger.apply("10");
+        System.out.println(num);
+
+        BiPredicate<List<String>, String> checkName = List::contains;
+        System.out.println(checkName.test(list, "Rimuru"));
+    }
+}
