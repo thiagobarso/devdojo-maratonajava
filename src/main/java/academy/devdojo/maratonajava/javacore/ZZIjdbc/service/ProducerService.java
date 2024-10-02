4ec853f1aef33baeabeb6089f86432e7f08b5152
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-10-01 21:51:23 -0300

    259 - JDBC pt 08 - Deletando dados com Statement

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/service/ProducerService.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.ZZIjdbc.service;
+
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.domnio.Producer;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+
+public class ProducerService {
+    public static void save(Producer producer){
+        ProducerRepository.save(producer);
+    }
+
+    public static void delete(int id){
+        if(id <= 0){
+            throw new IllegalArgumentException("Invalid value for id");
+        }
+        ProducerRepository.delete(id);
+    }
+}
--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -1,18 +1,14 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.domnio.Producer;
-import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
         Producer producer = Producer.builder().name("Studio Deen").build();
-        ProducerRepository.save(producer);
-//        log.info("info");
-//        log.debug("debug");
-//        log.warn("warn");
-//        log.error("error");
-//        log.trace("trace");
+//        ProducerService.save(producer);
+        ProducerService.delete(3);
     }
 }
