7f3b491e138ef59f09546a00b113a1de889c404b
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-10-01 21:35:25 -0300

    258 - JDBC pt 07 - Lombok e Log4J2

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -2,19 +2,21 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.repository;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.domnio.Producer;
+import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class ProducesRepository {
+@Log4j2
+public class ProducerRepository {
 
     public static void save(Producer producer) {
         String sql = "insert into `anime_store`.`producer` (`name`) values ('%s');".formatted(producer.getName());
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
-            System.out.println(rowsAffected);
+            log.info("Inserted producer in the database, rows affected '{}'", rowsAffected);
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -1,11 +1,18 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.domnio.Producer;
-import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducesRepository;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("NHK").build();
-        ProducesRepository.save(producer);
+        Producer producer = Producer.builder().name("Studio Deen").build();
+        ProducerRepository.save(producer);
+//        log.info("info");
+//        log.debug("debug");
+//        log.warn("warn");
+//        log.error("error");
+//        log.trace("trace");
     }
 }
