f70239395d325913d7d9374254d54a65f7ce57ad
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-10-04 20:44:38 -0300

    284 - Pattern Matching for instanceof

---
+++ src/test/java/academy/devdojo/maratonajava/javacore/ZZKjunit/dominio/DeveloperTest.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.ZZKjunit.dominio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeveloperTest {
+
+    @Test
+    public void instanceOf_ExecutesChildClassMethod_WhenObjectsIsOfChildType() {
+        Employee employeeDeveloper = new Developer("1", "Java");
+        if (employeeDeveloper instanceof Developer) {
+            Developer developer = (Developer) employeeDeveloper;
+            Assertions.assertEquals("Java", developer.getMainLanguage());
+        }
+
+        if (employeeDeveloper instanceof Developer developer) {
+            Assertions.assertEquals("Java", developer.getMainLanguage());
+        }
+    }
+
+}
